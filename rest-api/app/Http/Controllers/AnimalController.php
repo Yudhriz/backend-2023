ae56005358e8a3f222391280ea30021454d3ab59
Yudhriz <riskyyudha678@gmail.com>
2023-10-20 11:30:27 +0700

    New Animal Controller

--- rest-api/app/Http/Controllers/AnimalController.php
+++ rest-api/app/Http/Controllers/AnimalController.php
@@ -24,24 +24,56 @@ class AnimalController extends Controller
     // }
 
     // Percobaan Ketiga
+    // public function index() {
+    //     echo "Menampilkan data animals";
+    // }
+
+    // public function store(Request $request) {
+    //     echo "Nama hewan: $request->nama";
+    //     echo "<br>";
+    //     echo "Menambahkan hewan baru";
+    // }
+
+    // public function update(Request $request, $id) {
+    //     echo "Nama hewan : $request->nama";
+    //     echo "<br>";
+    //     echo "Mengupate data hewan id $id";
+    // }
+
+    // public function destroy($id) {
+    //     echo "Menghapus data hewan id $id";
+    // }
+
+    // Percobaan Keempat
+
+    public $animals = ["Beruang","Bebek","Ikan"];
     public function index() {
-        echo "Menampilkan data animals";
+        echo "Menampilkan seluruh hewan <br>";
+        # gunakan foreach untuk menampilkan data animals (array)
+        foreach ($this->animals as $animal) {
+            echo "- $animal <br>";
+        }
     }
 
     public function store(Request $request) {
-        echo "Nama hewan: $request->nama";
-        echo "<br>";
-        echo "Menambahkan hewan baru";
+        echo "Menambahkan hewan baru <br>";
+        array_push($this->animals, $request->animal);
+
+        $this->index();
     }
 
     public function update(Request $request, $id) {
-        echo "Nama hewan : $request->nama";
-        echo "<br>";
-        echo "Mengupate data hewan id $id";
+        echo "Mengupate data hewan id $id <br>";
+        $this->animals[$id] = $request->animal;
+
+        $this->index();
     }
 
     public function destroy($id) {
         echo "Menghapus data hewan id $id";
+        unset($this->animals[$id]);
+
+        $this->index();
     }
 
 }
