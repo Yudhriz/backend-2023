001f29bf417044d6fff9104b93b1d81a7e875fde
Yudhriz <riskyyudha678@gmail.com>
2023-12-01 10:32:51 +0700

    Praktikum Pertemuan 9 Membuat data Fruits

--- pertemuan-9/data/fruits.js
+++ pertemuan-9/data/fruits.js
@@ -3,7 +3,7 @@
  * - Buat array yang berisi data buah.
  * - Refactor variable ke ES6 Variable.
  */
-var fruits;
+const fruits = ["Jeruk", "Apel", "Durian"];
 
 // TODO 2: export variable fruits
-module.exports = "export kode di sini";
+module.exports = fruits;
