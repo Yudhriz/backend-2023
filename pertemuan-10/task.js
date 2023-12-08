2148501a2ce8851acaef69b96ff89b60757c7aa0
Yudhriz <riskyyudha678@gmail.com>
2023-12-08 10:12:35 +0700

    Task Done

--- pertemuan-10/task.js
+++ pertemuan-10/task.js
@@ -2,23 +2,57 @@
  * Fungsi untuk menampilkan hasil download
  * @param {string} result - Nama file yang didownload
  */
-function showDownload(result) {
-  console.log("Download selesai");
-  console.log("Hasil Download: " + result);
-}
+// function showDownload(result) {
+//   console.log("Download selesai");
+//   console.log("Hasil Download: " + result);
+// }
+
+
+/**
+ * Fungsi untuk show download
+ * Function promise
+ */
+const showDownload = () => {
+  return new Promise((resolve) => {
+    resolve("Download selesai");
+  });
+};
 
 /**
  * Fungsi untuk download file
  * @param {function} callback - Function callback show
  */
-function download(callShowDownload) {
-  setTimeout(function () {
-    const result = "windows-10.exe";
-    callShowDownload(result);
-  }, 3000);
-}
+// function download(callShowDownload) {
+//   setTimeout(function () {
+//     const result = "windows-10.exe";
+//     callShowDownload(result);
+//   }, 3000);
+// }
 
-download(showDownload);
+// download(showDownload);
+
+/**
+ * Fungsi untuk download file
+ * Function promise
+ */
+const downloadFile = () => {
+  return new Promise((resolve) => {
+    const result = "windows-10.exe";
+    setTimeout(() => {
+      resolve("Hasil Download: " + result);
+    }, 3000);
+  });
+};
+
+/**
+ * Function Async Await
+ */
+const download = async () => {
+  console.log(await showDownload());
+  console.log(await downloadFile());
+};
+
+download();
 
 /**
  * TODO:
